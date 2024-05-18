1b5f90784a31dd6bbf0f2ca8da88e4d629855850
rodis00 <happyekkov@gmail.com>
2024-05-18 15:18:23 +0200

    add task_1

---
+++ src/main/java/org/example/sscmastertask/SscMasterTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.sscmastertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscMasterTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SscMasterTaskApplication.class, args);
+    }
+
+}
