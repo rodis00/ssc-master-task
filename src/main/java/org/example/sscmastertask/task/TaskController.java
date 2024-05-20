9eada8885a27055ed1692b330315c75707d79073
rodis00 <happyekkov@gmail.com>
2024-05-20 02:16:22 +0200

    add cors

--- src/main/java/org/example/sscmastertask/task/TaskController.java
+++ src/main/java/org/example/sscmastertask/task/TaskController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
 
     private final TaskService taskService;
--- src/main/java/org/example/sscmastertask/user/UserController.java
+++ src/main/java/org/example/sscmastertask/user/UserController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserService userService;
